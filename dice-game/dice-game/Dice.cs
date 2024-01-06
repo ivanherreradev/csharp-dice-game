90fe3af909c4ac39c8dc1b32ee541267a9db0bc8
ivanherreradev <herreraivan@outlook.com.ar>
2024-01-06 15:49:49 -0300

    project completed

---
+++ dice-game/dice-game/Dice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieSet
+{
+    public class Die
+    {
+        private int[] Numbers { get; set; }
+
+        public Die()
+        {
+            Numbers = new int[6] { 1, 2, 3, 4, 5, 6 };
+        }
+
+        public int Roll()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 6);
+
+            return Numbers[randomNumber];
+        }
+    }
+}
---
+++ dice-game/dice-game/Program.cs
@@ -0,0 +1,44 @@
+﻿using DieSet;
+
+Console.WriteLine("Welcome to the game of betting against the dice!");
+
+int bet;
+bool follow = true;
+
+while (follow)
+{
+    Console.WriteLine("\nEnter your bet: ");
+    string input = Console.ReadLine();
+
+    if (int.TryParse(input, out bet))
+    {
+        if (bet > 0 && bet < 7)
+        {
+            Die die = new Die();
+
+            int dieResult = die.Roll();
+
+            if (dieResult == bet)
+            {
+                Console.WriteLine($"Congratulations!! you have won!\nYour bet: {bet}\nDie result: {dieResult}");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry! the house has won!\nYour bet: {bet}\nDie Result: {dieResult}");
+            }
+
+            follow = false;
+        }
+        else
+        {
+            Console.WriteLine("Enter only numbers between 1 to 6! Please try again: ");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input! Please try again: ");
+    }
+}
+
+Console.WriteLine("Thank you for playing!");
+
